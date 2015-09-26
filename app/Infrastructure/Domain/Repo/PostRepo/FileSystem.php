ce7c62cf003afbe65673ebbd31e3509fd21c5ca6
Michael Flanagan <michael@flanagan.ie>
2015-09-26 03:59:04 +0100

    pretty print post details json

--- app/Infrastructure/Domain/Repo/PostRepo/FileSystem.php
+++ app/Infrastructure/Domain/Repo/PostRepo/FileSystem.php
@@ -9,13 +9,13 @@ use League\Flysystem\Adapter\Local;
 class FileSystem implements \App\Domain\Repo\PostRepo
 {
     private $filesystem;
-    
+
     public function __construct()
     {
         $adapter = new Local(public_path().'/post');
         $this->filesystem = new FlyFileSystem($adapter);
     }
-    
+
     public function all()
     {
         $post_folders = $this->filesystem->listContents();
@@ -25,21 +25,21 @@ class FileSystem implements \App\Domain\Repo\PostRepo
             $id = new ValueObject\UUID($folder['path']);
             $posts[] = $this->fetch($id);
         }
-        
+
         usort($posts, function(ValueObject\Post $post_a, ValueObject\Post $post_b){
             return $post_a->date()->lt($post_b->date());
         });
-        
+
         return $posts;
     }
 
     public function latest()
     {
         $posts = $this->all();
-        
+
         return array_pop($posts);
     }
-    
+
     /**
      * @param \App\Domain\ValueObject\UUID $id
      * @return \App\Domain\ValueObject\Post
@@ -52,28 +52,28 @@ class FileSystem implements \App\Domain\Repo\PostRepo
         $title = new ValueObject\String\NonBlank($details->title);
         $author = new ValueObject\String\NonBlank($details->author);
         $date = ValueObject\Date\Past::parse($details->date);
-        
+
         $content = new ValueObject\String\NonBlank(
             $this->filesystem->read($this->content_file($id))
         );
-        
+
         return new ValueObject\Post($id, $title, $author, $date, $content);
     }
-    
+
     private function details_filename(ValueObject\UUID $id)
     {
         return $id."/details.json";
     }
-    
+
     private function content_file(ValueObject\UUID $id)
     {
         return $id."/post.md";
     }
-       
+
     public function store(ValueObject\Post $post)
     {
         $details = FileSystem\Details::make($post);
-        $this->filesystem->write($this->details_filename($post->id()), json_encode($details));
+        $this->filesystem->write($this->details_filename($post->id()), json_encode($details, JSON_PRETTY_PRINT));
         $this->filesystem->write($this->content_file($post->id()), $post->content()->value());
     }
 }
